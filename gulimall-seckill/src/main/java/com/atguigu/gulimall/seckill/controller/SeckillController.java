4445952751d76fd2d2006b9e473dc8a6c895a833
wangtianshun <wangtianshun@heilangroup.cn>
2020-12-16 19:32:47 +0800

    商城业务-秒杀服务-页面渲染完成

--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/controller/SeckillController.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/controller/SeckillController.java
@@ -4,10 +4,9 @@ import com.atguigu.common.utils.R;
 import com.atguigu.gulimall.seckill.service.SeckillService;
 import com.atguigu.gulimall.seckill.to.SeckillSkuRedisTo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +16,7 @@ import java.util.List;
  * @Date: 2020/12/13 19:16
  * @Version 1.0
  */
-@RestController
+@Controller
 public class SeckillController {
 
     @Autowired
@@ -26,6 +25,7 @@ public class SeckillController {
      * 返回当前时间可以参与的秒杀商品信息
      * @return
      */
+    @ResponseBody
     @GetMapping(value = "/getCurrentSeckillSkus")
     public R getCurrentSeckillSkus() {
         //获取到当前可以参加秒杀商品的信息
@@ -33,7 +33,7 @@ public class SeckillController {
 
         return R.ok().setData(vos);
     }
-
+    @ResponseBody
     @GetMapping("/sku/seckill/{skuId}")
     public R getSkuSecKillInfo(@PathVariable("skuId") Long skuId){
         SeckillSkuRedisTo to = seckillService.getSkuSecKillInfo(skuId);
@@ -41,13 +41,15 @@ public class SeckillController {
     }
 
     @GetMapping("/kill")
-    public R seckill(@RequestParam("killId") String killId,
+    public String seckill(@RequestParam("killId") String killId,
                      @RequestParam("key") String key,
-                     @RequestParam("num") Integer num){
+                     @RequestParam("num") Integer num,
+                     Model model){
         // 1、判断是否登录(登录拦截器已经自动处理)
 
         String orderSn = seckillService.kill(killId, key, num);
-        return R.ok().setData(orderSn);
+        model.addAttribute("orderSn", orderSn);
+        return "success";
     }
 
 }
